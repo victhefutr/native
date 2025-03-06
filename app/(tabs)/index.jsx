93a4fd6901dea3894483d46616762fad34cd2f8e
Chukwuemeka Victor Nweze <128430080+victhefutr@users.noreply.github.com>
2025-03-06 03:14:10 +0000

    make changes to index.tsx

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,38 @@
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import icedCoffee from '@/assets/images/iced-coffee.jpg'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={icedCoffee}
+        resizeMode='cover'
+        style={styles.image}
+      >
+        <Text style={styles.text}>Marketplace</Text>
+        </ImageBackground>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 42,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+})
